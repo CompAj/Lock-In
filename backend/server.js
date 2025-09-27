be823c2778e1597dc8b6058a500be209bea60c4d
CompAj <114016409+CompAj@users.noreply.github.com>
2025-09-27 17:30:39 -0600

    Implemented accesss policies
    Made endpoints for access policies

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express'; 
-import bodyParser from 'body-parser';
-import Cloudflare from 'cloudflare';
-import dotenv from 'dotenv';
-
+import express from "express";
+import bodyParser from "body-parser";
+import Cloudflare from "cloudflare";
+import dotenv from "dotenv";
+import crypto from "crypto";
 dotenv.config();
 
 const app = express();
@@ -13,27 +13,43 @@ const client = new Cloudflare({
   apiToken: process.env.API_TOKEN,
 });
 
+app.get("/create-policy", async (req, res) => {
+  const user = req.body;
 
-app.get('/create-policy', async (req, res) => {
-    const user = req.body;
+  const gatewayRule = await client.zeroTrust.gateway.rules.create({
+    account_id: process.env.ACCOUNT_ID,
+    action: "block",
+    name: crypto.randomUUID(),
+    description: "",
+    enabled: true,
+    filters: ["dns"],
+    traffic: "any(dns.content_category[*] in {149})",
+    identity: `${user.email}`,
+    device_posture: "",
+  });
 
-    const gatewayRule = await client.zeroTrust.gateway.rules.create({
-        account_id: process.env.ACCOUNT_ID,
-        action: "block",
-        name: "Block Stuff2",
-        description: "",
-        enabled: true,
-        filters: ["dns"],
-        traffic: 'any(dns.content_category[*] in {149})',
-        identity: `${user.email}`,
-        device_posture: ""
+  res.json(gatewayRule);
+});
 
-    });
+app.post("/create-access-policy", async (req, res) => {
+  const { email } = req.body;
 
-    res.json(gatewayRule)
+  const policy = await client.zeroTrust.access.policies.create({
+    account_id: process.env.ACCOUNT_ID,
+    decision: "allow",
+    name: `Access Policy for ${email}`,
+    include: [
+      {
+        email: {
+          email,
+        },
+      },
+    ],
+  });
+
+  res.json(policy);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
