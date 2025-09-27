886aaf1e6b06edbfdccb2522cb9526685557d781
CompAj <114016409+CompAj@users.noreply.github.com>
2025-09-27 13:22:46 -0600

    Nativewind install with glueui

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-bold text-3xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
